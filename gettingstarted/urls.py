c66167a21107c99e2f7cad4dbfed5585040ccbbf
Herman TKPD <herman.wahyudi@tokopedia.com>
2015-04-14 14:49:56 +0700

    Update urls.py

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', automation.views.index, name='index'),
-    url(r'^db', automation.views.db, name='db'),
+    #url(r'^db', automation.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login', automation.views.login, name='login'),
     url(r'^logout', automation.views.logout, name='logout'),
