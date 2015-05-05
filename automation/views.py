020a057836fdf501bba8cc2bf6a5673dd934d92a
Herman TKPD <herman.wahyudi@tokopedia.com>
2015-05-05 15:03:47 +0700

    Migrate huge

--- automation/views.py
+++ automation/views.py
@@ -2,28 +2,43 @@ from django.shortcuts import render, render_to_response, redirect
 from django.core.context_processors import csrf
 from django.http import HttpResponse
 
-from .models import Greeting
+#from .models import Greeting
 
 # Create your views here.
 def index(request):
 	if 'sess_key' not in request.session:
 		c = {}
 		c.update(csrf(request))
-		return render(request, 'login.html', c)
+		return render(request, 'login1.html', c)
 	else:
 		var = {}
-		if not request.user.is_authenticated():
+		if request.user.is_authenticated():
 			var = request.session['sess_email']
-		return render_to_response('home.html', {'email':var})
+		return render_to_response('dashboard.html', {'email':var})
 
 def login(request):
-	email = request.POST['email']
-	passw = request.POST['passw']
-	if email == "tkpd.qc@gmail.com" and passw == "1234asdf":
-		request.session['sess_key'] = email + "&" + passw
-		request.session['sess_email'] = email
-		request.session['sess_passw'] = passw
-	return redirect('/')
+	if request.POST:
+		email = request.POST['email']
+		passw = request.POST['passw']
+		data_email = ['tkpd.qc@gmail.com', 'herman.wahyudi02@gmail.com']
+		is_found_email = False
+		for c in data_email:
+			if c == email:
+				is_found_email = True
+				break
+		if is_found_email and passw == "1234asdf":
+			request.session['sess_key'] = email + "&" + passw
+			request.session['sess_email'] = email
+			request.session['sess_passw'] = passw
+			return redirect('/')
+		else:
+			c = {}
+			c.update(csrf(request))
+			error = "Email or password is incorrect!"
+			c.update({'error':error})
+			return render_to_response('login1.html', c)
+	else:
+		return redirect('/')
 
 def logout(request):
 	del request.session['sess_key']
